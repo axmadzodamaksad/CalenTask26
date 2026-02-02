0eae5a11576314e0aa6b7e932234558c7beea503
maks <axmadzodamaksad@gmail.com>
2026-02-02 02:07:05 -0500

    Create initial TaskTimerApp structure
    Initial setup of the TaskTimer application with main entry point.

---
+++ TaskTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTimerApp.swift
+//  TaskTimer
+//
+//  Created by Ahmadzoda Maqsad on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
